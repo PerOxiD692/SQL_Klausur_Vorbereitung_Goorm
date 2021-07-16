bb7ac64437476a94760bdd8f81c30853b17df826
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-07-16 10:37:51 +0000

    SQL Script angelegt; calls angelegt;

---
+++ SQL_Vorbereitung/Probeklausur.sql
@@ -0,0 +1,22 @@
+/* Vorbereitung auf die Klausur */
+-- Vorbereitung der Datenbank
+DROP DATABASE IF EXISTS mydb;
+CREATE DATABASE IF NOT EXISTS mydb;
+USE mydb;
+SHOW DATABASES;
+
+/* Vorbereitung der Tabelle
+DROP TABLE IF EXISTS test;
+CREATE TABLE IF NOT EXISTS test( #Es muss mindestens eine Spalte angegeben werden!
+    name VARCHAR(45), # Jeder Spalte muss ein Datentyp zugewiesen werden!
+    age INT
+);
+DESCRIBE test; #Zeigt Strucktur der Tabelle!
+SELECT * FROM test; #Zeigt alle Einträge der Tabelle!
+*/
+
+
+
+
+
+
